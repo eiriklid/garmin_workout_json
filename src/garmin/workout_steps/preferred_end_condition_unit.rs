84e0a7ef1f1938614c038269d6a0de3bc394df5d
Eirik Lid <eiriklid@gmail.com>
2025-04-15 21:30:39 +0200

    Update PreferredEndConditionUnit

--- src/garmin/workout_steps/preferred_end_condition_unit.rs
+++ src/garmin/workout_steps/preferred_end_condition_unit.rs
@@ -1,31 +1,60 @@
-use serde::{Deserialize, Serialize};
+use serde::ser::SerializeStruct;
+use serde::{Deserialize, Serialize, Serializer};
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all = "snake_case")]
+pub enum Unit{
+    Meter
+}
+
+
+#[derive(Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct PreferredEndConditionUnit {
-    /*
-    Todo:
-        - Enum of type keys
-        - Derive id and display_order based on key
-     */
-    unit_id: u8,
-    unit_key: String,
-    factor: f32
+    unit_key: Unit,
 }
+
+impl PreferredEndConditionUnit {
+
+    pub fn unit_id(&self) -> u8{
+        match self.unit_key {
+            Unit::Meter => 1,
+        }
+    }
+
+    pub fn factor(&self) -> f32{
+        match self.unit_key {
+            Unit::Meter => 100.0,
+        }
+    }
+}
+
+
 
 impl Default for PreferredEndConditionUnit {
     fn default() -> Self {
         PreferredEndConditionUnit{
-            unit_id: 1,
-            unit_key: "meter".to_string(),
-            factor: 100.0
+            unit_key: Unit::Meter,
         }
+    }
+}
+
+
+impl Serialize for PreferredEndConditionUnit {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut state = serializer.serialize_struct("PreferredEndCondition", 3)?;
+        state.serialize_field("unitId", &self.unit_id())?;
+        state.serialize_field("unitKey", &self.unit_key)?;
+        state.serialize_field("factor", &self.factor())?;
+        state.end()
     }
 }
 
 #[cfg(test)]
 mod tests {
-
     use super::*;
 
     #[test]
@@ -38,17 +67,15 @@ mod tests {
           }
         "#;
         let json: PreferredEndConditionUnit = serde_json::from_str(json_str).unwrap();
-        assert_eq!(json.unit_id, 1);
-        assert_eq!(json.unit_key, "meter");
-        assert_eq!(json.factor, 100.0);
+        assert_eq!(json.unit_id(), 1);
+        assert_eq!(json.unit_key, Unit::Meter);
+        assert_eq!(json.factor(), 100.0);
     }
 
     #[test]
     fn test_serialize_step() {
         let step = PreferredEndConditionUnit {
-            unit_id: 1,
-            unit_key: "meter".to_string(),
-            factor: 100.0
+            unit_key: Unit::Meter,
         };
         let json_str = serde_json::to_string(&step).unwrap();
         let expected = r#"{"unitId":1,"unitKey":"meter","factor":100.0}"#;
